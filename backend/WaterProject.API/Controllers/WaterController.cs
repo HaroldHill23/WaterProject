6516c38a7bd1d9bf6aa8822a04bafa138563bd8f
HaroldHill23 <caseythompson165@gmail.com>
2025-03-18 18:18:23 -0600

    Initial Commit

---
+++ backend/WaterProject.API/Controllers/WaterController.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using WaterProject.API.Data;
+
+namespace WaterProject.API.Controllers
+{
+    [Route("[controller]")]
+    [ApiController]
+    public class WaterController : ControllerBase //Controller Base = Generic Controller
+    {
+
+        private WaterDbContext _waterContext;
+        public WaterController(WaterDbContext temp)     //Constructor, can also use lamda here: public WaterController(WaterDbContext temp) => _waterContext = temp;
+        {   
+            _waterContext = temp;
+        }
+
+
+        [HttpGet("AllProjects")]
+        public IEnumerable<Project> GetProjects()
+        {
+            return _waterContext.Projects.ToList();
+        }
+
+        [HttpGet("FunctionalProjects")]
+        public IEnumerable<Project> GetFunctionalProjects()
+        {
+            var something = _waterContext.Projects.Where(p => p.ProjectFunctionalityStatus == "Functional").ToList();
+            return something;
+        }
+
+    }
+}
+
